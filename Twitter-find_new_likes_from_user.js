06c571729eba0af40a062b4e0d15f085163c9f11
Cuicui <bykajnev@hotmail.com>
2021-09-16 23:55:06 -0300

    update!
    now it sends me that tweet as image to my telegram's ID

--- Twitter-find_new_likes_from_user.js
+++ Twitter-find_new_likes_from_user.js
@@ -1,77 +1,87 @@
-var id='ID';
-var TOKEN='';
+const token = 'token';
+var id = 'put the id here';
+var seconds = 60;
+const Bearer_token = 'put your token here';
+const chat_id ='put your Telegram chat id here';
 
-
-var first=1;//no sé pero lo tuve que hacer asi
+const bot = new TelegramBot(token, { polling: true });
+const TelegramBot = require('node-telegram-bot-api');
+var first = 1;
 var request = require('request');
 const fs = require('fs');
+
 var options = {
-  'method': 'GET',
-  'url': 'https://api.twitter.com/2/users/'+ id + '/liked_tweets',
-  'headers': {
-    'Authorization': 'Bearer ' + TOKEN;
-  }
+    'method': 'GET',
+    'url': 'https://api.twitter.com/2/users/' + id + '/liked_tweets',
+    'headers': {
+        'Authorization': 'Bearer ' + Bearer_token
+    }
 };
 
 var lastID = '0';
-var seconds = 10;
 
 // get the first tweet and store it in memory
-request(options, function (error, response) {
+request(options, function(error, response) {
     if (error) throw new Error(error);
     var obj = JSON.parse(response.body);
-    
+
     fs.readFile('latest.txt', (err, data) => {
-      if (err){
-        lastID=obj.data[10]['id'];
-        throw err;
-      }
-      lastID=data;
-      if (lastID =='1' ) lastID=obj.data[10]['id'];
+        if (err) {
+            lastID = obj.data[1]['id'];
+            throw err;
+        }
+        lastID = data;
+        if (lastID == '1') lastID = obj.data[1]['id'];
     });
 
- });
+});
 
-function find_new_liked_tweets(){
+function find_new_liked_tweets() {
   
-  console.log("checking..");
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
+    request(options, function(error, response) {
+        if (error) throw new Error(error);
 
-    var obj = JSON.parse(response.body);
-    var count=-1;
+        var obj = JSON.parse(response.body);
+        var count = -1;
+        var isPhoto = 0;
+        // find how much tweet should update
+        for (let i = 0; i < obj.data.length; i++) {
 
-    // find how much tweet should update
-    for (let i = 0; i < obj.data.length ; i++) {
-     
-      if ((lastID === obj.data[i]['id'])){
-        count=i;
-        break;
-      }
-    }
-    
-    if (count == -1) count=5;
-
-    // print all new tweets
-    if (count > 0 ){
-      for (let j = count-1; j >= 0 ; j--) {
-
-        if (first <= 0){
-          console.log("New --> " + obj.data[j]['text']);
+            if ((lastID === obj.data[i]['id'])) {
+                count = i;
+                break;
+            }
         }
-        lastID=obj.data[j]['id'];
-      }
 
-      first=0;
+        if (count == -1) count = 1;
 
-      fs.writeFile('latest.txt', lastID , (err) => {
-        if (err)
-          throw err;
-      });
-    }
-  });
+        if (count > 0) {
+            for (let j = count - 1; j >= 0; j--) {
 
+                if (first <= 0) {
+                    var url = 'https://platform.twitter.com/embed/index.html?dnt=false&embedId=twitter-widget-0&frame=true&hideCard=false&hideThread=true&id=' + obj.data[j]['id'] + '&lang=en&theme=dark&widgetsVersion=ed20a2b%3A1601588405575';
+                    const Pageres = require('pageres');
+                    (async () => {
+                        await new Pageres({ delay: 1 })
+                            .src(url, ['iphone 5s'], { crop: true, transparent: true, darkMode: true, filename: obj.data[j]['id'] })
+                            .dest('')
+                            .run();
+                        bot.sendPhoto(chat_id, obj.data[j]['id'] + '.png', { caption: obj.data[j]['text'] });
+                        function deletephoto() {
+                            fs.unlinkSync(obj.data[j]['id'] + '.png');
+                        }
+                        setTimeout(deletephoto, 20000);
+
+                    })();
+                }
+                lastID = obj.data[j]['id'];
+            }
+            first = 0;
+            fs.writeFile('latest.txt', lastID, (err) => {
+                if (err)
+                    throw err;
+            });
+        }
+    });
 };
-
-// interval of X seconds
 setInterval(find_new_liked_tweets, seconds * 1000);
